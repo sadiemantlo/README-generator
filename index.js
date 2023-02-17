af1883a641da118b41f0703f16c64fba0115990e
Sadie Mantlo <sadiemantlo@Sadies-MacBook-Air.local>
2023-02-16 16:55:43 -0800

    added rought readme mockup and prompt questions

--- index.js
+++ index.js
@@ -3,10 +3,62 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+const userInput = () => {
+    return inquirer.propmt ([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Write a description of your project, remember, this is added to your README so spelling and punctuation matter.'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the installation requirements for your project?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Write a description on how to use your project.',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Write out the contribution guidelines for your project.',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Write instructions on how to run all necessary tests.',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license for your project: ',
+            choices: ['MIT', 'Apache', 'GPL', 'none'],
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub username.',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email.',
+        }
+    ])
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeFile(fileName, data) {
+    fs.writeFile('')
+}
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,44 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# **${data.title}** 
+
+  ## **Description**
+
+  ${data.description}
+
+  ## **Table of Contents
+
+  [Installation](#installation)
+  [Usage](#usage)
+  [License](#license)
+  [Contributing](#contributing)
+  [Tests](#tests)
+  [Questions](#questions)
+
+  ## **Installation**
+
+  ${data.installtion}
+
+  ## **Usage**
+
+  ${data.usage}
+
+  ## **License**
+
+  ${data.license}
+
+  ## **Contributing**
+
+  ${data.contributing}
+
+  ## **Tests**
+
+  ${data.tests}
+
+  ## **Questions**
+
+  If you have any questions, feel free to contact me via [GitHub](${data.github}) or my email: ${data.email}.
 
 `;
 }
