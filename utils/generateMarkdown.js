95ec8bf0cffdadd7aef4c49041e4690bd5d0d6bc
Sadie Mantlo <sadiemantlo@Sadies-MacBook-Air.local>
2023-02-20 20:15:18 -0800

    edited readme markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,20 @@
 function generateMarkdown(data) {
+
   let gitUser = 'https://github.com/' + data.github;
   let licenseDisplay;
   let licenseLink;
+
   if (data.license === 'MIT' ? licenseDisplay = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
     : data.license === 'Apache' ? licenseDisplay = '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)'
     : data.license === 'GPL' ? licenseDisplay = '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)'
     : data.license === 'none' ? licenseDisplay = ''
     : console.log('crying'))
-  console.log(licenseDisplay);
+
   if (data.license === 'MIT' ? licenseLink = '[MIT License](https://opensource.org/licenses/MIT)'
     : data.license === 'Apache' ? licenseLink = '[Apache License](https://opensource.org/licenses/Apache-2.0)'
     : data.license === 'GPL' ? licenseLink = '[GPL v3 License](https://www.gnu.org/licenses/gpl-3.0)'
     : data.license === 'none' ? licenseLink = 'N/A'
     : console.log('crying'))
-  console.log(licenseLink);
   return `# **${data.title}** ${licenseDisplay}
 
   ## **Description** 
@@ -51,7 +52,8 @@ function generateMarkdown(data) {
 
   ## **Questions**
 
-  If you have any questions, feel free to contact me via [GitHub](${gitUser}) or my email: ${data.email}.
+  If you have any questions, feel free to contact me via email: ${data.email}.
+  Here is a link to my [GitHub](${gitUser}).
 
 `;
 }
